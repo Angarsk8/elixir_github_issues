2299456c4746e8590332ad50196ba0d4f5d2bf50
Angarsk8 <andresa.garciah621@gmail.com>
2015-12-27 18:32:07 -0500

    add proper logging to our project (set info as the minumum logging level)

--- lib/issues/issues_github.ex
+++ lib/issues/issues_github.ex
@@ -1,8 +1,12 @@
 defmodule Issues.GithubIssues do
+
+	require Logger
+
 	@issues_url Application.get_env :issues, :github_url
 	@user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
 
 	def fetch(user, project) do
+		Logger.info "Starts fetching #{user}'s project #{project}"
 		github_url(user, project)
 		|> HTTPoison.get(@user_agent)
 		|> handle_response
@@ -11,12 +15,15 @@ defmodule Issues.GithubIssues do
 	def github_url(user, project), do: "#{@issues_url}/repos/#{user}/#{project}/issues"
 
 	def handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+		Logger.info "Succesfull response"
 		{:ok, :jsx.decode body}
 	end	
-	def handle_response({:ok, %HTTPoison.Response{status_code: ___, body: body}}) do
+	def handle_response({:ok, %HTTPoison.Response{status_code: status, body: body}}) do
+		Logger.error "Error: #{status} returned"
 		{:not_usable, :jsx.decode body}
 	end
 	def handle_response({:error, %HTTPoison.Error{reason: reason}}) do
+		Logger.error "Error: #{reason}"
 		{:error, reason}
 	end
 end
